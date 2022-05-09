c0563af2b35aa8551c01c489e5468f048f619b5b
周子文 <xuehf@mdlytz.com>
2022-05-09 17:21:01 +0800

    add code

---
+++ src/com/aladingziben/devops/devops.groovy
@@ -0,0 +1,16 @@
+/**
+ * @author: herrywen
+ * @Date: 2022-05-09
+ */
+
+package com.aladingziben.devops;
+
+//格式化输出
+def PrintMes(value,color){
+    colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'blue'  : "\033[47;34m ${value} \033[0m",
+              'green' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
+    ansiColor('xterm') {
+        println(colors[color])
+    }
+}
---
+++ vars/hello.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+/**
+ * @author: herrywen
+ * @Date: 2022-05-09
+ */
+
+def call(){
+    println("hello")
+}
