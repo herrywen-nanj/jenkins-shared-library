038efc34965d84540a43d219676eb15e5a2720c8
周子文 <xuehf@mdlytz.com>
2022-05-10 17:58:09 +0800

    更改jenkinsfile，增加调用代码

---
+++ vars/devops.groovy
@@ -0,0 +1,43 @@
+#!/usr/bin/env groovy
+/**
+ * @author: herrywen
+ * @Date: 2022-05-09
+ */
+import com.aladingziben.devops.FormatPrint
+def run(params) {
+
+    pipeline {
+        agent any
+        options {
+            // 禁止同时运行多个流水线
+            disableConcurrentBuilds()
+        }
+        environment {
+            examples_var1 = sh(script: 'echo "当前的时间是: `date`"', returnStdout: true).trim()
+        }
+        stages{
+            stage("声明式流水线: 拉一个代码") {
+                steps {
+                    git url: "${params.git_url}"
+                }
+            }
+            stage("声明式流水线: 执行一个命令"){
+                steps {
+                    script {
+                        def tools = new FormatPrint()
+                        tools.PrintMes("获取代码",'black')
+                        sh "${params.cmd}"
+                    }
+                }
+            }
+            stage("声明式流水线: 打印现在的时间"){
+                steps {
+                    script {
+                        inputMsg([id:"${params.input_id}",msg:"${params.input_msg}"])
+                    }
+
+                }
+            }
+        }
+    }
+}
