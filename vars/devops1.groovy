a8d819a02b537d3d7233f9a0335b31f41f11a86e
herrywen1 <13585162465@163.com>
2022-05-26 21:13:22 +0800

    change directory

--- vars/devops1.groovy
+++ vars/devops1.groovy
@@ -6,7 +6,7 @@ pipeline {
         stage ('read') {
             steps {
                 script {
-                    def yaml_file = libraryResource 'com/aladingziben/devops/PiplineCfg.yaml'
+                    def yaml_file = libraryResource('PiplineCfg.yaml')
                     //def data = readYaml file : '/opt/PiplineCfg.yaml'
                     def data = readYaml txt : yaml_file
                     println data.get(defaultBranchName).get(project_name).get('PRE_FIX')
