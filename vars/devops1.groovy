666c4799aacf6fc1df48c44cfa36a38d54373d49
herrywen1 <13585162465@163.com>
2022-05-26 22:17:05 +0800

    adasda1

--- vars/devops1.groovy
+++ vars/devops1.groovy
@@ -8,12 +8,7 @@ pipeline {
         stage ('read') {
             steps {
                 script {
-		    //println("$defaultBranchName")
                     def yaml_file = libraryResource('PiplineCfg.yaml')
-                    //println yaml_file
-
-		    //def data = readYaml file : '/opt/PiplineCfg.yaml'
-		    //sh "echo $pwd && ls -l"
                     def data = readYaml text : yaml_file
                     println data.get(defaultBranchName).get(project_name).get('PRE_FIX')
                }
