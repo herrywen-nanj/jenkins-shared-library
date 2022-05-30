160d18fe3a66359f5dc4ea1c2bb4819cb269d4e7
周子文 <xuehf@mdlytz.com>
2022-05-30 10:20:45 +0800

    update 7

--- src/com/aladingziben/devops/GitServer.groovy
+++ src/com/aladingziben/devops/GitServer.groovy
@@ -7,12 +7,14 @@ package com.aladingziben.devops
 
 
 def CheckOutCode(BRANCHNAME) {
-    checkout([$class                           : 'GitSCM', branches: [[name: BRANCHNAME]],
+    /*checkout([$class                           : 'GitSCM', branches: [[name: BRANCHNAME]],
               doGenerateSubmoduleConfigurations: false,
               extensions                       : [],
               submoduleCfg                     : [],
               userRemoteConfigs                : [[credentialsId: 'gitee_account', url: "${env.GIT_URL}"]]])
-
+	
+    */
+    git branch: BRANCHNAME, credentialsId: 'gitee_account', url: "${GIT_URL}"
     dir("${env.workspace}"){
         env.GIT_COMMIT = sh(script: "git log -1 --pretty=%B | cat", returnStdout: true).trim()
     }
