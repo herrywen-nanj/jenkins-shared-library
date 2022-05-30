ce6a0b7572e7e0f82f529261ef993d99d84f72fc
周子文 <xuehf@mdlytz.com>
2022-05-30 09:57:04 +0800

    update1

--- vars/devops.groovy
+++ vars/devops.groovy
@@ -73,6 +73,7 @@ def call() {
                     script {
                         println("---------------$defaultBranchName--------------------")
                         CfgMessage.GetCfg(defaultBranchName,project_name)
+						println("---------------${env.GIT_URL)------------------------")
                         
                     }
                 }
