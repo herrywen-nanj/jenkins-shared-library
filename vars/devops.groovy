8028b8c9139dca79aafda296b53edf1b3b9fb270
周子文 <xuehf@mdlytz.com>
2022-05-30 16:27:48 +0800

    update code

--- vars/devops.groovy
+++ vars/devops.groovy
@@ -42,7 +42,7 @@ def call() {
 
 
 
-	parameters {
+		parameters {
             gitParameter (
                     defaultValue: defaultBranchName,
                     branchFilter: 'origin/(.*)',
