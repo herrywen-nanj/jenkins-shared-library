c07680726ff1b98ee9842c737a1db5ad0b5d3011
周子文 <10349853+herrywen1@user.noreply.gitee.com>
2022-05-31 09:18:11 +0000

    update src/com/aladingziben/devops/BranchName.groovy.

--- src/com/aladingziben/devops/BranchName.groovy
+++ src/com/aladingziben/devops/BranchName.groovy
@@ -1,3 +1,11 @@
+/**
+ * @author: herrywen
+ * @Date: 2022-05-31
+ */
+
+package com.aladingziben.devops
+
+
 def GetDefaultBranchName(String env) {
 		if (env == 'prod') {
 			defaultBranchName = "master"
