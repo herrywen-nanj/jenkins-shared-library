3d089a528104f985a2269b6d1c1aa846364286f7
herrywen1 <13585162465@163.com>
2022-06-01 01:10:39 +0800

    resolv NullPointer4

--- vars/devops2.groovy
+++ vars/devops2.groovy
@@ -28,7 +28,7 @@ def call() {
     def (Environment_Prefix, project_name) = "${env.JOB_BASE_NAME}".split("-")
     def defaultBranchName = BranchName.GetDefaultBranchName(Environment_Prefix)
     println("---------------------------------$defaultBranchName-------------------------------------")
-    CfgMessage.GetCfg("$defaultBranchName",project_name)
+    CfgMessage.GetCfg("${defaultBranchName}","${project_name}")
     pipeline {
         agent any
         tools {
